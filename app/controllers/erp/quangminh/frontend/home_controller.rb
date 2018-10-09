e1b11b628b6a3eb3effef20e4c4c95150a4619e9
Nguyen Ngoc Son <sonnn@hoangkhang.com.vn>
2018-10-09 06:33:28 +0000

    update

--- app/controllers/erp/quangminh/frontend/home_controller.rb
+++ app/controllers/erp/quangminh/frontend/home_controller.rb
@@ -2,7 +2,7 @@ module Erp
   module Quangminh
     module Frontend
       class HomeController < Erp::Frontend::FrontendController
-        def index          
+        def index
         end
       end
     end
