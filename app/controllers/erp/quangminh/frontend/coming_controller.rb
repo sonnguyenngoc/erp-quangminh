1dacd986b0fff83922da5dd304086328448f663b
Ngoc Son <sonnn@hoangkhang.com.vn>
2019-02-21 15:26:25 +0000

    update

--- app/controllers/erp/quangminh/frontend/coming_controller.rb
+++ app/controllers/erp/quangminh/frontend/coming_controller.rb
@@ -7,7 +7,7 @@ module Erp
         end
         def not_found          
           render(:status => 404)
-          render :layout => 'erp/frontend/coming'
+          redirect_to erp_hoanmy.root_path
         end
       end
     end
